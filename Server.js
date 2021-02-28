cdcc62c81bfd3ac9e4c354b9356e2a7c644bd291
Pachara-Pc <pachara.mee@domre.tu.ac.th>
2021-02-28 10:30:05 +0700

    update

--- Server.js
+++ Server.js
@@ -43,10 +43,10 @@ app.get("/ControlValve/:value",(req,res)=>{
         const status = Value.split(",");
         if(status[0] == Calculate.getcountpump() && status[1] == Calculate.getvalvestatus()?0:1 ){
             console.log(`Arduino number pump : ${status[0] }, Arduino pump status ${status[1] } `);
-            console.log(`Server  number pump : ${Calculate.getcountpump()}, Server pump status ${Calculate.getvalvestatus()?0:1} `);
+            console.log(`Server  number pump : ${Calculate.getcountpump()}, Server  pump status ${Calculate.getvalvestatus()?1:0}  Server  pump status ${Calculate.getvalvestatus()}`);
         }else{
             console.log(`Arduino number pump : ${status[0] }, Arduino pump status ${status[1] } `);
-            console.log(`Server  number pump : ${Calculate.getcountpump()}, Server pump status ${Calculate.getvalvestatus()?0:1} `);
+            console.log(`Server  number pump : ${Calculate.getcountpump()}, Server  pump status ${Calculate.getvalvestatus()?1:0}  Server  pump status ${Calculate.getvalvestatus()}`);
         }
         
         res.send('0');
