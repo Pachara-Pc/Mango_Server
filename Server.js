7b5fdbfe2dc31fbf2d2736534f70363de4505f5c
Pachara-Pc <pachara.mee@domre.tu.ac.th>
2021-02-28 07:37:58 +0700

    update

--- Server.js
+++ Server.js
@@ -45,12 +45,12 @@ app.get("/ControlValve/:value",(req,res)=>{
         
       if( (Controlpump.getZone_1()||Controlpump.getZone_2() ) == true  ){
 
-            console.log(`IF status[0] = ${status[0]} Calculate.getcountpump() = ${Calculate.getcountpump()} status[1] = ${status[1]} Calculate.getvalvestatus()?0:1 = ${Calculate.getvalvestatus()}`);
-            res.send(`${Calculate.getcountpump()},${Calculate.getvalvestatus()}`)
+            console.log(`IF status[0] = ${status[0]} Calculate.getcountpump() = ${Calculate.getcountpump()} status[1] = ${status[1]} Calculate.getvalvestatus()?0:1 = ${Calculate.getvalvestatus()?0:1}`);
+            res.send(`${Calculate.getcountpump()},${Calculate.getvalvestatus()?0:1}`)
         }
         else{
-            console.log(`ELSE status[0] = ${status[0]} Calculate.getcountpump() = ${Calculate.getcountpump()} status[1] = ${status[1]} Calculate.getvalvestatus()?0:1 = ${Calculate.getvalvestatus()}`);
-            res.send(`${Calculate.getcountpump()},${Calculate.getvalvestatus()}`)
+            console.log(`ELSE status[0] = ${status[0]} Calculate.getcountpump() = ${Calculate.getcountpump()} status[1] = ${status[1]} Calculate.getvalvestatus()?0:1 = ${Calculate.getvalvestatus()?0:1}`);
+            res.send(`${Calculate.getcountpump()},${Calculate.getvalvestatus()?0:1}`)
             confirmRequest = true;
         }
 
