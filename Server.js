9ae58b3c9b474c81b3f8a26be54219cd5733c4ab
Pachara-Pc <pachara.mee@dome.tu.ac.th>
2021-06-05 14:20:37 +0700

    test update new Server

--- Server.js
+++ Server.js
@@ -207,10 +207,10 @@ setInterval(()=>{
         const Time = new Date();
 
                 // if(Time.getHours() === getTimehour() && Time.getMinutes() === getTimeninute() && Time.getSeconds() === getTimesecond() && Calculate.getIrrigation() === 0 &&  StatusServer === true){
-                if( Time.getMinutes()% 2 === 0 & Time.getSeconds()%30 === 0 ){
+                if( Time.getMinutes()% 1 === 0 & Time.getSeconds() === 0 ){
 
                      
-                         Calculate.Calculate_round_1()
+                        //  Calculate.Calculate_round_1()
                    
                         console.log("Check");
                        // console.log(`${Time.getHours()}:${Time.getMinutes()}:${Time.getSeconds()} `);
