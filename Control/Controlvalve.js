ad40ac02fb9ec8818e3711255b555f78b12cd271
Pachara-Pc <pachara.mee@dome.tu.ac.th>
2021-06-09 14:21:22 +0700

    update

--- Control/Controlvalve.js
+++ Control/Controlvalve.js
@@ -129,7 +129,7 @@ setInterval(() => {
     }
   }
 //Time.getHours() === 0 &&
-  if ( Time.getMinutes() == 16 && Time.getSeconds() === 0 && Calculate.getCheckDayonValue() === "watered") {
+  if ( Time.getMinutes() == 30 && Time.getSeconds() === 0 && Calculate.getCheckDayonValue() === "watered") {
     
     Calculate.setCheckDayonValue("notwatered")
     Calculate.getZone() === 1 ? Calculate.setZone(2) : Calculate.setZone(1)
